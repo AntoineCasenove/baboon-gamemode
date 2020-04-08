771a94f6f60941b0cc3adc89935129fe32d8306b
Antoine <antoine.casenove@viacesi.fr>
2020-04-08 16:55:53 +0200

    replace pistol weapon by 357 magnum & add shotgun (#1)

--- gamemode/player_class/player_custom.lua
+++ gamemode/player_class/player_custom.lua
@@ -15,8 +15,10 @@ DEFINE_BASECLASS( "player_default" )
 
  	self.Player:RemoveAllAmmo()
 
- 	self.Player:GiveAmmo( 256,	"Pistol", true )
- 	self.Player:Give( "weapon_pistol" )
+ 	self.Player:GiveAmmo( 256,	"357", true )
+ 	self.Player:GiveAmmo( 256,	"Buckshot", true )
+ 	self.Player:Give( "weapon_357" )
+ 	self.Player:Give( "weapon_shotgun" )
 
  end
 
